97a23e0b809ca8ca3affb7a58c7544d77bda5422
Hitarth Patel <31387675+Gitarth@users.noreply.github.com>
2020-10-01 18:03:51 -0400

    First coding challenge

---
+++ randomChar.py
@@ -0,0 +1,33 @@
+# First Challenge
+# October 1, 2020
+# Author: Hitarth Patel
+# 
+# Objective: Given a stream of elements too large to store in memory. Pick a ran# random element from the stream with uniform probability
+import random
+import os
+import sys
+
+def getCharacter(theString):
+    "Function will take givenString as an input and return a random character from that string"
+    try:
+        finalChar = random.choice(theString)
+        # Check for if the choice is empty
+        while finalChar.isspace() or len(finalChar) == 0:
+            finalChar = random.choice(theString)
+
+        return print(f"The random character is {finalChar}")
+    except IndexError:
+        # print(err)
+        print("Can't pick from empty string, try again.")
+    
+
+if __name__ == "__main__":
+    while True:
+
+        givenString = input("Please enter a String:\t")
+        if givenString.isspace():
+            print("String cannot be just spaces.")
+            break
+        getCharacter(givenString)
+
+
